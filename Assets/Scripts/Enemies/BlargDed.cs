12b26c6012694d1f9f4c74fbfefe4493ff4a228d
Vincent Prime <myself@vincentprime.com>
2019-01-26 15:37:01 -0700

    Dissolve shader effect when spiders die

--- Assets/Scripts/Enemies/BlargDed.cs
+++ Assets/Scripts/Enemies/BlargDed.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class BlargDed : MonoBehaviour
 {
+    
+    Material mat;
+    private bool dead = false;
+    private float deadTime = 0f;
+    public float deathEffectTime = 3f;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        if (dead)
+        {
+            float effect = (Time.time - deadTime) / deathEffectTime;
+            mat.SetFloat("_DissolveAmount", effect);
+            if (effect >= 1f)
+            {
+                Destroy(gameObject);
+            }
+        }
+            
+    }
+
 
     public void Die()
     {
+        dead = true;
         transform.rotation = Quaternion.Inverse(transform.rotation);
+        deadTime = Time.time;
     }
 }
