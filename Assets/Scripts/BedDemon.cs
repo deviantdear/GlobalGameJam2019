c0d6e250a44006b3fd017215ec80cffc3dd2eadf
vadams4 <vadams4@asu.edu>
2019-01-26 23:23:00 -0700

    Bed demon scripts, lighting adjustments and fog addition

--- Assets/Scripts/BedDemon.cs
+++ Assets/Scripts/BedDemon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sun : MonoBehaviour {
+public class BedDemon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +11,6 @@ public class Sun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.right, 2f * Time.deltaTime);
-        transform.LookAt(Vector3.zero);
+		
 	}
-
 }
